c458f8f559040d3b9e5bdfad6146ab023b04a175
artemy-bakulin <logic2000.bakulin@gmail.com>
2023-07-08 00:13:00 +0300

    stable

--- catran.py
+++ catran.py
@@ -1,16 +1,158 @@
+from abc import abstractmethod
+from typing import List, Tuple
+
+import numpy as np
+from causalscbench.models.abstract_model import AbstractInferenceModel
+from causalscbench.models.training_regimes import TrainingRegime
+
+import lightning.pytorch as pl
+import networkx as nx
+
+
 import torch
 from torch import nn
 import torch.functional as F
 from torch.utils.data import random_split, DataLoader
 from torch.optim.lr_scheduler import MultiStepLR
-import lightning.pytorch as pl
 from lion_pytorch import Lion
+from pytorch_metric_learning.samplers import  MPerClassSampler
 
 import pandas as pd
-import numpy as np
 
 
 
+
+
+class Dataset(pl.LightningDataModule):
+    def __init__(self, expression_matrix, gene_names, interventions=None, regime='interventional', batch_size=2048, num_workers=1, train_split=0.9):
+
+        super().__init__()
+                
+        self.expression_matrix = expression_matrix
+        self.batch_size = batch_size
+        self.train_split = train_split
+        self.num_workers = num_workers
+        
+        
+        
+        self.genes = np.array(gene_names)
+        self.genes, expr_mask = np.unique(gene_names, return_index=True)
+        
+        
+        self.expression_matrix = expression_matrix[:, expr_mask]
+
+        self.n_genes = len(self.genes)
+        self.n_cells = self.expression_matrix.shape[0]
+        
+        
+        self.interventions = np.array(interventions)
+        self.regime = regime
+            
+            
+        if self.regime == 'interventional':
+            
+            
+            get_gene_idx = lambda x: np.where(self.genes == x)[0][0] if x in self.genes else -1
+
+            pert_idxs = pd.Series(self.interventions).apply(get_gene_idx).values
+            
+            self.expression_matrix = np.concatenate((self.expression_matrix, pert_idxs[:, None]), axis=1)
+            
+            
+            
+    def setup(self, stage=None):    
+        self.train = self.expression_matrix
+        
+
+    def train_dataloader(self):
+        sampler = MPerClassSampler((self.train[:, -1]), 25)
+
+        return DataLoader(self.train,
+                          batch_size=self.batch_size,
+                          num_workers=self.num_workers,
+                          sampler=sampler, drop_last=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.val,
+                          batch_size=self.batch_size,
+                          num_workers=self.num_workers,
+                          shuffle=False)
+    
+
+
+
+class MyModel(AbstractInferenceModel):
+    
+    def __init__(self,
+                batch_size=2000,
+                N_SAMPLE_GENES=500,
+                LR = .0001,
+                WD = 0.05,
+                PERC_HIDDEN = 0.80,
+                NUM_EPOCHS = 50,
+                top_n_edges = 1_000,
+                accelerator='gpu'):
+        super().__init__()
+        
+        self.batch_size=batch_size
+        self.N_SAMPLE_GENES=N_SAMPLE_GENES
+        self.LR = LR
+        self.WD=WD
+        self.PERC_HIDDEN = PERC_HIDDEN
+        self.NUM_EPOCHS = NUM_EPOCHS
+        self.top_n_edges = top_n_edges
+        self.accelerator = accelerator
+        
+    
+    
+    def __call__(
+        self,
+        expression_matrix: np.array,
+        interventions: List[str],
+        gene_names: List[str],
+        training_regime: TrainingRegime,
+        seed: int = 0,
+    ) -> List[Tuple]:
+                
+
+        data = Dataset(expression_matrix, gene_names, batch_size=self.batch_size, interventions=interventions)
+        
+        
+        N_SAMPLE_GENES = min(self.N_SAMPLE_GENES, len(data.genes))
+        
+        model = Catran(data.genes, N_SAMPLE_GENES, lr=self.LR, perc_hidden=self.PERC_HIDDEN, wd=self.WD)
+        
+        trainer = pl.Trainer(min_epochs=1, max_epochs=self.NUM_EPOCHS, accelerator=self.accelerator, limit_val_batches=0)
+        
+        trainer.fit(model, data)
+        
+        
+        model.calculate_attention_matrix()
+        att = pd.DataFrame(model.attention_matrix, index=data.genes, columns=data.genes)
+        att.iloc[range(len(att)), range(len(att))] = 0
+                
+        att = np.maximum(att, att.T)
+        att.iloc[:, :] = np.triu(att)
+        
+        nth_el = np.sort(att.values.flatten())[-self.top_n_edges]
+
+        Adj = (att >= nth_el).astype(int)
+                
+            
+        
+        edges = []
+        with open('edges.txt', 'w+') as o:
+            for gene_A in Adj.index:
+                for gene_B in Adj.columns:
+                    if Adj.loc[gene_A, gene_B]:
+                        edges.append((gene_A, gene_B))
+                        o.write(gene_A + '_' + gene_B + '\n')
+                    
+                            
+        return edges
+    
+    
+    
 def init_weights(m, activation='relu'):
     if isinstance(m, nn.BatchNorm1d):
         nn.init.constant_(m.weight.data, 1)
@@ -23,11 +165,11 @@ def init_weights(m, activation='relu'):
             if m.bias is not None:
                 nn.init.constant_(m.bias.data, 0)
         elif isinstance(m, nn.Linear):
-            m.weight.data.normal_(0, 0.001)
+            m.weight.data.normal_(0, 0.0001)
             if m.bias is not None:
                 nn.init.constant_(m.bias.data, 0) 
         elif isinstance(m, nn.Embedding):
-            m.weight.data.normal_(0, 0.001)
+            m.weight.data.normal_(0, 0.1)
           
     else:
         if isinstance(m, nn.Linear):
@@ -40,8 +182,8 @@ def init_weights(m, activation='relu'):
 class MaskedAttention(nn.Module):
     def __init__(self, input_dim):
         super().__init__()
-        self.ll = nn.Linear(input_dim, input_dim)
-        self.ll2 = nn.Linear(input_dim, input_dim)
+        #self.ll = nn.Linear(input_dim, input_dim)
+        #self.act = nn.SiLU()
         
         
         
@@ -57,8 +199,10 @@ class MaskedAttention(nn.Module):
 
         return func
                     
-    def forward(self, x, y, hidden_idxs=None, function='sigmoid'):
-        x = self.ll(x)       
+    def forward(self, x, y, hidden_idxs=None, function='softmax'):
+        
+        #x = self.ll(x)
+        #x = self.act(x)
         
         
         if x.dim() == 3:
@@ -108,17 +252,18 @@ class AttBlock(nn.Module):
         #attn_output1 = (attn_output1 + x)  / 2 ** 0.5
         attn_output1 = self.bn_attn1(attn_output1)
 
-        ll_output1 = self.ll1(attn_output1)
+        #ll_output1 = self.ll1(attn_output1)
         #ll_output1 = (attn_output1 + ll_output1)  / 2 ** 0.5
-        ll_output1 = self.bn1(ll_output1)
-        ll_output1 = self.act(ll_output1)
+        #ll_output1 = self.bn1(ll_output1)
+        #ll_output1 = self.act(ll_output1)
         
-        return ll_output1
+        return attn_output1
     
     
 class Catran(pl.LightningModule):
     def __init__(self, genes, n_genes_in_minibatch, 
-                     embed_size=20, lr=0.001, perc_hidden=0.25, wd=0.01, regime='interventional'):
+                embed_size=40, lr=0.001, perc_hidden=0.25, wd=0.01, use_2nd_att=False,
+                kappa=0.4, phi=1, interventional=True):
         super().__init__()
         self.genes = genes
         self.n_genes = len(self.genes)
@@ -128,7 +273,11 @@ class Catran(pl.LightningModule):
         self.wd = wd
         self.perc_hidden = perc_hidden
         self.calculate_attention = True
-        self.regime = regime
+        self.kappa = kappa
+        self.phi = phi
+        self.interventional = interventional
+        
+        self.use_2nd_att = use_2nd_att
         
         self.act = nn.SiLU()
         self.embedding = nn.Embedding(self.n_genes, embed_size)
@@ -143,6 +292,7 @@ class Catran(pl.LightningModule):
         self.attn = MaskedAttention(self.embed_size)
        
         self.att_block1 = AttBlock(self.n_genes_in_minibatch, self.embed_size+1)
+        self.att_block2 = AttBlock(self.n_genes_in_minibatch, self.embed_size+1)
 
 
         self.ll_prefin = nn.Linear(self.embed_size+1, self.embed_size+1)
@@ -155,7 +305,7 @@ class Catran(pl.LightningModule):
                              [self.embedding, self.attn, 
                               self.ll0, self.bn0,
                               self.ll_pre, self.bn_pre,
-                              self.att_block1,                           
+                              self.att_block1, self.att_block2,                           
                               self.ll_prefin, self.bn_prefin, self.ll_fin]
                     )
         
@@ -165,18 +315,18 @@ class Catran(pl.LightningModule):
                 
         
     def _common_step(self, batch, batch_idx):
-        x = batch.float()
-        if self.regime == 'interventional':
-            interventions = x[:, -1].int()
-            x = x[:, :-1]
-                
+        x = batch.float().to('cpu')
         
+        if self.interventional:
+            interventions = x[:, -1].int().to(self.device)
+            x = x[:, :-1]
+                        
         gene_mask = self._generate_random_mask(self.n_genes, self.n_genes_in_minibatch)
-        gene_idxs = torch.where(gene_mask)[0]
-        x = x[:, gene_mask]
+        gene_idxs = torch.where(gene_mask)[0].to(self.device)
+        x = x[:, gene_mask].to(self.device)
 
         self.hidden_mask = self._generate_2D_random_mask(self.n_genes_in_minibatch, 
-                                     int(self.n_genes_in_minibatch * self.perc_hidden), len(x))
+                                     int(self.n_genes_in_minibatch * self.perc_hidden), len(x)).to(self.device)
         
             
         x_corrupted = torch.clone(x)
@@ -186,11 +336,11 @@ class Catran(pl.LightningModule):
         
         
             
-        loss =  .7 * self.loss_fn(preds[self.hidden_mask], x[self.hidden_mask]) + \
-                    .3 * self.loss_fn(preds[~self.hidden_mask], x[~self.hidden_mask])
+        loss =  self.loss_fn(preds[self.hidden_mask], x[self.hidden_mask]) + \
+                    self.kappa * self.loss_fn(preds[~self.hidden_mask], x[~self.hidden_mask])
         
-        if self.regime == 'interventional':
-            loss += self._calculate_interventional_loss(x, preds, self.hidden_mask, attn_weights, interventions, gene_idxs)
+        if self.interventional:
+            loss += 10 * self._calculate_interventional_loss(x, preds, self.hidden_mask, attn_weights, interventions, gene_idxs)
   
         return loss, preds[self.hidden_mask], x[self.hidden_mask]
     
@@ -213,17 +363,20 @@ class Catran(pl.LightningModule):
                 non_interv_x = x[interventions!=interven]
                 interv_preds = preds[interventions==interven]
                 non_interv_preds = preds[interventions!=interven]
-                
-                
+                                
                 m1 = huber(interv_x, interv_preds).mean(0)
                 m2 = huber(non_interv_x, non_interv_preds).mean(0)
 
                 
                 coefs = sigmoid(m2 / m1 - 1)
                 
-                l += mse(attention_matrix[:, gene_idxs==interven], coefs).mean()
+                l += mse(attention_matrix[:, gene_idxs==interven].squeeze(1), coefs).mean()
+                
+                
                 
         l = l / (len(intervention_types) - 1)
+        
+        
 
         return l
     
@@ -242,16 +395,26 @@ class Catran(pl.LightningModule):
         
         x = self.ll0(x)
         x = self.bn0(x)
+        
+#         x = self.act(x)
                             
-        x = self.ll_pre(x)
-        x = self.bn_pre(x)
+#         x = self.ll_pre(x)
+#         x = self.bn_pre(x)
         
         attn_output1 = self.att_block1(x, attn_weights)
-
-        combined = (attn_output1) 
+        combined = attn_output1
+        for i in range(10):
+            combined =  self.att_block1(combined, attn_weights)
+        #combined = self.att_block1(combined, attn_weights)
+        #combined = self.att_block1(combined, attn_weights)
+        
+        if self.use_2nd_att:
+            attn_output2 = self.att_block1(x, attn_weights)
+            combined += attn_output2
         
         combined = self.ll_prefin(combined)
-
+        combined = self.bn_prefin(combined)
+        combined = self.act(combined)
         
         res = self.ll_fin(combined)
         res = res.squeeze(2)
@@ -262,7 +425,7 @@ class Catran(pl.LightningModule):
     def _generate_random_mask(self, total_size, sample_size):
         mask = torch.full((total_size,), False, dtype=bool)
         mask[:sample_size] = True
-        mask = mask[torch.randperm(total_size)].to(self.device)
+        mask = mask[torch.randperm(total_size)]
         return mask
     
     def _generate_2D_random_mask(self, total_size, sample_size, bs):
@@ -280,6 +443,8 @@ class Catran(pl.LightningModule):
     def calculate_attention_matrix(self):
         x_emb = self.embedding(torch.arange(self.n_genes).to(self.device))
         self.attention_matrix = self.attn(x_emb, x_emb, function='softmax')
+        
+        
         self.attention_matrix = self.attention_matrix.detach().cpu().numpy()
         
     def training_step(self, batch, batch_idx):
@@ -302,7 +467,7 @@ class Catran(pl.LightningModule):
         loss, scores, y = self._common_step(batch, batch_idx)
         return scores, y
     
-    def configure_optimizers(self, optimizer='lion', use_scheduler=True):
+    def configure_optimizers(self, optimizer='lion', use_scheduler=False):
         if optimizer=='adamw':
             optimizer = torch.optim.AdamW(self.parameters(), lr=self.lr, weight_decay=self.wd)
         elif optimizer=='lion':
@@ -311,10 +476,8 @@ class Catran(pl.LightningModule):
             scheduler = MultiStepLR(optimizer, milestones=[100], gamma=0.01)
             return [optimizer], [{"scheduler": scheduler, "interval": "epoch"}]
         else:
-            return optimizer
+            return [optimizer]
     
-    def lr_scheduler_step(self, scheduler, metric):
-        scheduler.step()
+#     def lr_scheduler_step(self, scheduler, metric):
+#         scheduler.step()
         
-    
-    
